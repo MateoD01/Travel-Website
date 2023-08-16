fdb05390b38c302b04c41debeb304efd43477d39
MateoD01 <mdaran646@gmail.com>
2023-08-15 22:06:42 -0300

    project finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@ window.addEventListener('scroll', scrollUp)
 const sections = document.querySelectorAll('section[id]')
 
 const scrollActive = () =>{
-    const scrollY = window.pageYOffset
+    const scrollY = window.scrollY
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
@@ -66,5 +66,19 @@ const scrollActive = () =>{
         }
     })
 }
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 400,
+    // reset: true // Animations repeat
+})
+
+sr.reveal('.home__data, .explore__data, .explore__user, .footer__container')
+sr.reveal('.home__card', {delay: 600, distance: '100px', interval: 100})
+sr.reveal('.about__data, .join__image', {origin: 'right'})
+sr.reveal('.about__image, .join__data', {origin: 'left'})
+sr.reveal('.popular__card', {interval: 200})
